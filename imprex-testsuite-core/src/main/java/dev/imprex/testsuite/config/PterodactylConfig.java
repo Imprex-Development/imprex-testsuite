63d1e3e487106682455d9fe22c5ef84667a68dd3
Nils Gereke <nilsgereke1@gmail.com>
2026-03-04 01:12:47 +0100

    wip: pelican changes

--- imprex-testsuite-core/src/main/java/dev/imprex/testsuite/config/PterodactylConfig.java
+++ imprex-testsuite-core/src/main/java/dev/imprex/testsuite/config/PterodactylConfig.java
@@ -3,6 +3,8 @@ package dev.imprex.testsuite.config;
 public record PterodactylConfig(String url, String applicationToken, String clientToken) {
 
 	public boolean valid() {
-		return url.startsWith("http");
+		return url.startsWith("http") &&
+				this.applicationToken.startsWith("papp_") &&
+				this.clientToken.startsWith("pacc_");
 	}
 }
